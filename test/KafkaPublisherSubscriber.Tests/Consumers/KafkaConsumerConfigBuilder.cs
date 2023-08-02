0cfb624b6b42d4e253343f03ab3dc27a150e15d2
André Lara Amorim <andre.amorim.si@gmail.com>
2023-08-02 14:23:21 -0300

    fix: changes testes consumer config builder

--- test/KafkaPublisherSubscriber.Tests/Consumers/KafkaConsumerConfigBuilder.cs
+++ test/KafkaPublisherSubscriber.Tests/Consumers/KafkaConsumerConfigBuilder.cs
@@ -5,124 +5,148 @@ using Xunit;
 
 namespace KafkaPublisherSubscriber.Tests.Consumers
 {
-    public class KafkaProducerConfigBuilderTests
+    public class KafkaConsumerConfigBuilderTests
     {
         [Fact]
         public void Test_ValidConfiguration()
         {
-            var builder = new KafkaProducerConfigBuilder();
-            var config = builder.WithBootstrapServers("localhost:9092")
-                                .WithTopic("my_topic")
-                                .WithEnableIdempotence(true)
+            var builder = new KafkaConsumerConfigBuilder();
+            var config = builder.WithGroupId("my_group")
+                                .WithBootstrapServers("localhost:9092")
+                                .WithEnableAutoCommit(true)
+                                .WithStatisticsIntervalMs(5000)
+                                .WithSessionTimeoutMs(60000)
+                                .WithEnablePartitionEof(false)
                                 .WithApiVersionRequest(true)
-                                .WithMessageSendMaxRetries(3)
-                                .WithAcks(Acks.All)
-                                .WithMaxInFlight(100)
-                                .Build();
-
-            // Assert expected values of the 'config' object
-            Assert.Equal("localhost:9092", config.BootstrapServers);
-            Assert.Equal("my_topic", config.Topic);
-            Assert.True(config.EnableIdempotence);
-            Assert.True(config.ApiVersionRequest);
-            Assert.Equal(3, config.MessageSendMaxRetries);
-            Assert.Equal(Acks.All, config.Acks);
-            Assert.Equal(100, config.MaxInFlight);
-        }
-
-        [Fact]
-        public void Test_MinimumRequiredConfigurations()
-        {
-            var builder = new KafkaProducerConfigBuilder();
-            var config = builder.WithBootstrapServers("localhost:9092")
                                 .WithTopic("my_topic")
+                                .WithTopicRetry("retry_topic")
+                                .WithTopicDeadLetter("dead_letter_topic")
+                                .WithMaxRetryAttempts(3)
+                                .WithAutoOffsetReset(AutoOffsetReset.Latest)
                                 .Build();
 
             // Assert expected values of the 'config' object
+            Assert.Equal("my_group", config.GroupId);
             Assert.Equal("localhost:9092", config.BootstrapServers);
+            Assert.True(config.EnableAutoCommit);
+            Assert.Equal(5000, config.StatisticsIntervalMs);
+            Assert.Equal(60000, config.SessionTimeoutMs);
+            Assert.False(config.EnablePartitionEof);
+            Assert.True(config.ApiVersionRequest);
             Assert.Equal("my_topic", config.Topic);
-            Assert.False(config.EnableIdempotence); // Default value for bool type is false
-            Assert.False(config.ApiVersionRequest); // Default value for bool type is false
-            Assert.Equal(0, config.MessageSendMaxRetries); // Default value for int type is 0
-            Assert.Equal(Acks.All, config.Acks); // Default value is Acks.All
-            Assert.Equal(0, config.MaxInFlight); // Default value for int type is 0
+            Assert.Equal("retry_topic", config.TopicRetry);
+            Assert.Equal("dead_letter_topic", config.TopicDeadLetter);
+            Assert.Equal(3, config.MaxRetryAttempts);
+            Assert.Equal(AutoOffsetReset.Latest, config.AutoOffsetReset);
         }
 
         [Fact]
         public void Test_InvalidConfigurations()
         {
-            var builder = new KafkaProducerConfigBuilder();
+            var builder = new KafkaConsumerConfigBuilder();
 
             // Missing 'bootstrapServers'
-            Assert.Throws<ArgumentNullException>(() => builder.WithTopic("my_topic").Build());
+            Assert.Throws<ArgumentNullException>(() => builder.WithGroupId("my_group").WithTopic("my_topic").Build());
+
+            // Missing 'groupId'
+            Assert.Throws<ArgumentNullException>(() => builder.WithBootstrapServers("localhost:9092").WithTopic("my_topic").Build());
 
             // Missing 'topic'
-            Assert.Throws<ArgumentNullException>(() => builder.WithBootstrapServers("localhost:9092").Build());
+            Assert.Throws<ArgumentNullException>(() => builder.WithBootstrapServers("localhost:9092").WithGroupId("my_group").Build());
+        }
+
+        [Fact]
+        public void Test_MinimumRequiredConfigurations()
+        {
+            var builder = new KafkaConsumerConfigBuilder();
+            var config = builder.WithGroupId("my_group")
+                                .WithBootstrapServers("localhost:9092")
+                                .WithTopic("my_topic")
+                                .Build();
+
+            // Assert expected values of the 'config' object
+            Assert.Equal("my_group", config.GroupId);
+            Assert.Equal("localhost:9092", config.BootstrapServers);
+            Assert.False(config.EnableAutoCommit); // Default value for bool type is false
+            Assert.Equal(0, config.StatisticsIntervalMs); // Default value for int type is 0
+            Assert.Equal(0, config.SessionTimeoutMs); // Default value for int type is 0
+            Assert.False(config.EnablePartitionEof); // Default value for bool type is false
+            Assert.False(config.ApiVersionRequest); // Default value for bool type is false
+            Assert.Equal(AutoOffsetReset.Earliest, config.AutoOffsetReset); // Default value is AutoOffsetReset.Earliest
         }
 
         [Fact]
         public void Test_OptionalConfigurationsNotSet()
         {
-            var builder = new KafkaProducerConfigBuilder();
-            var config = builder.WithBootstrapServers("localhost:9092")
+            var builder = new KafkaConsumerConfigBuilder();
+            var config = builder.WithGroupId("my_group")
+                                .WithBootstrapServers("localhost:9092")
                                 .WithTopic("my_topic")
-                                .WithAcks(Acks.Leader)
+                                .WithMaxRetryAttempts(5)
                                 .Build();
 
             // Assert expected values of the 'config' object
+            Assert.Equal("my_group", config.GroupId);
             Assert.Equal("localhost:9092", config.BootstrapServers);
-            Assert.Equal("my_topic", config.Topic);
-            Assert.False(config.EnableIdempotence); // Default value for bool type is false
+            Assert.False(config.EnableAutoCommit); // Default value for bool type is false
+            Assert.Equal(0, config.StatisticsIntervalMs); // Default value for int type is 0
+            Assert.Equal(0, config.SessionTimeoutMs); // Default value for int type is 0
+            Assert.False(config.EnablePartitionEof); // Default value for bool type is false
             Assert.False(config.ApiVersionRequest); // Default value for bool type is false
-            Assert.Equal(0, config.MessageSendMaxRetries); // Default value for int type is 0
-            Assert.Equal(Acks.Leader, config.Acks);
-            Assert.Equal(0, config.MaxInFlight); // Default value for int type is 0
+            Assert.Equal("my_topic", config.Topic);
+            Assert.Null(config.TopicRetry); // Default value for string type is null
+            Assert.Null(config.TopicDeadLetter); // Default value for string type is null
+            Assert.Equal(5, config.MaxRetryAttempts);
+            Assert.Equal(AutoOffsetReset.Earliest, config.AutoOffsetReset); // Default value is AutoOffsetReset.Earliest
         }
 
         [Fact]
         public void Test_ChainingOfConfigurations()
         {
-            var builder = new KafkaProducerConfigBuilder();
-            var config = builder.WithBootstrapServers("localhost:9092")
+            var builder = new KafkaConsumerConfigBuilder();
+            var config = builder.WithGroupId("my_group")
+                                .WithBootstrapServers("localhost:9092")
                                 .WithTopic("my_topic")
-                                .WithMessageSendMaxRetries(5)
-                                .WithAcks(Acks.All)
+                                .WithSessionTimeoutMs(30000)
+                                .WithMaxRetryAttempts(2)
                                 .WithBootstrapServers("new_server:9092") // Overwriting 'bootstrapServers'
+                                .WithTopic("new_topic") // Overwriting 'topic'
                                 .Build();
 
             // Assert expected values of the 'config' object
+            Assert.Equal("my_group", config.GroupId);
             Assert.Equal("new_server:9092", config.BootstrapServers);
-            Assert.Equal("my_topic", config.Topic);
-            Assert.False(config.EnableIdempotence); // Default value for bool type is false
+            Assert.False(config.EnableAutoCommit); // Default value for bool type is false
+            Assert.Equal(0, config.StatisticsIntervalMs); // Default value for int type is 0
+            Assert.Equal(30000, config.SessionTimeoutMs);
+            Assert.False(config.EnablePartitionEof); // Default value for bool type is false
             Assert.False(config.ApiVersionRequest); // Default value for bool type is false
-            Assert.Equal(5, config.MessageSendMaxRetries);
-            Assert.Equal(Acks.All, config.Acks);
-            Assert.Equal(0, config.MaxInFlight); // Default value for int type is 0
+            Assert.Equal("new_topic", config.Topic);
+            Assert.Null(config.TopicRetry); // Default value for string type is null
+            Assert.Null(config.TopicDeadLetter); // Default value for string type is null
+            Assert.Equal(2, config.MaxRetryAttempts);
+            Assert.Equal(AutoOffsetReset.Earliest, config.AutoOffsetReset); // Default value is AutoOffsetReset.Earliest
         }
 
         [Fact]
-        public void Test_DifferentCombinationsOfAcks()
+        public void Test_DifferentAutoOffsetReset()
         {
-            var builder = new KafkaProducerConfigBuilder();
-            var config1 = builder.WithBootstrapServers("localhost:9092")
+            var builder = new KafkaConsumerConfigBuilder();
+            var config1 = builder.WithGroupId("my_group")
+                                 .WithBootstrapServers("localhost:9092")
                                  .WithTopic("my_topic")
-                                 .WithAcks(Acks.All)
+                                 .WithAutoOffsetReset(AutoOffsetReset.Earliest)
                                  .Build();
 
-            var config2 = builder.WithBootstrapServers("localhost:9092")
+            var config2 = builder.WithGroupId("my_group")
+                                 .WithBootstrapServers("localhost:9092")
                                  .WithTopic("my_topic")
-                                 .WithAcks(Acks.Leader)
-                                 .Build();
-
-            var config3 = builder.WithBootstrapServers("localhost:9092")
-                                 .WithTopic("my_topic")
-                                 .WithAcks(Acks.None)
+                                 .WithAutoOffsetReset(AutoOffsetReset.Latest)
                                  .Build();
 
             // Assert expected values of the 'config' objects
-            Assert.Equal(Acks.All, config1.Acks);
-            Assert.Equal(Acks.Leader, config2.Acks);
-            Assert.Equal(Acks.None, config3.Acks);
+            Assert.Equal(AutoOffsetReset.Earliest, config1.AutoOffsetReset);
+            Assert.Equal(AutoOffsetReset.Latest, config2.AutoOffsetReset);
         }
     }
 }
